4df2f25569b9201cee3f263f9647c563501c3858
Henrique L. Alves <henriquelalves@gmail.com>
2016-02-25 23:23:56 -0700

    Changed files and project names

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # GodotTIE
 A simple Text Interface Engine to control text output (like in a RPG dialogue) for Godot.
+
+The "Cave-Story" font was created by enigmansmp1824; it's CC-BY-SA licensed, and can be found in https://fontlibrary.org/pt/font/cave-story.
+
+## Features:
+
+* Control the velocity in which the text is going to be displayed! (Even text dialogues can have emotions!)
+* Adjust visually the Interface size, so it can fit it in any Dialogue box you want! The script will handle the maximum number of lines and characters!
+* Buffer texts, inputs, and breaks in the Dialogue, with easy to use methods!
+* Control the Dialogue flow from the outside with the available user signals!
+* Use tags on specific parts of your text! (In a RPG, you may want to show specific animations during certain parts of the dialogue; e.g. a "!" question mark popup in the head of a character in one of the moments of the dialogue)
+* User scroll friendly; Log all the text printed; auto-clip words; and more!
+
+Take a look at the 'public' methods in the script and the export variables available; it should give you a hint about the stuff you can easily customize in the engine!
+
+## Installation:
+You'll need the Godot Engine to do this.
+
+1. Clone or download this repository;
+2. Copy the folder "Text Interface Engine" to your Godot project;
+3. Instance "text_interface_engine.scn" as a node in the scene that you want the Text Interface.
+
+Done!
--- main.gd
+++ main.gd
@@ -1,13 +1,86 @@
-
 extends Control
 
-func _ready():
-	var tie = get_node("text_interface_engine")
-	tie.buff_text("HAIEAHOU EAOWUIEHO \nAWIE JOAWIE ", 0.04)
-	tie.buff_silence(2)
-	tie.buff_text("HAIEAHOU \nEAOWUIEHO AWIE JOAWIE ", 0.1)
-	tie.buff_text("HAIEAHOU EAOWUIEHO AWIE JOAWIE ", 0.04)
-	tie.buff_break()
-	tie.buff_text("HAIEAHOU EAOWUIEHO AWIE JOAWIE ", 0.04)
-	tie.buff_text("HAIEAHOU EAOWUIEHO AWIE JOAWIE ", 0.04)
+onready var tie = get_node("panel/text_interface_engine")
+
+func select_demo(i):
+	tie.reset()
+	if(i == 1):
+		# "A Beautiful Song"
+		# by Henrique Alves
+		tie.set_color(Color(1,1,1))
+		# Buff text: "Text", duration (in seconds) of each letter
+		tie.buff_text("This is a song, ", 0.1)
+		tie.buff_text("lalala\n", 0.2)
+		# Buff silence: Duration of the silence (in seconds)
+		tie.buff_silence(1)
+		tie.buff_text("It is so beautiful, ", 0.1)
+		tie.buff_text("lalala\n", 0.2)
+		tie.buff_silence(1)
+		tie.buff_text("I love this song, ", 0.1)
+		tie.buff_text("lalala\n", 0.2)
+		tie.buff_silence(1)
+		tie.buff_text("But now I'll ", 0.1) # WAIT FOR THE DROP
+		tie.buff_text("DROP", 0.02) # ?????
+		tie.buff_silence(0.4)
+		tie.buff_text(" THE BASS\n", 0.02) # !!!!!
+		tie.buff_silence(0.4)
+		tie.buff_text("TVUVTUVUTUVU WOODBODBOWBDODBO TUUVUTVU WODWVDOOWDV WODOWVOOWDVOWVD DUBDUBDUBUDUDB OWVDOWVWDOWVDOWVDOWVDWVDOWVDOWVODV", 0.04) # I firmly believe this particular verse is my Magna Opus.
+	elif(i == 2):
+		tie.set_color(Color(1,1,0.3))
+		# If velocity is 0, than whole text is printed 
+		tie.buff_text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras semper finibus sapien, ut fringilla nulla vehicula ac. In hac habitasse platea dictumst. Nulla lobortis tempus sem vel lobortis. Mauris facilisis mollis nunc, vitae aliquet dui dictum id. Nullam ultricies facilisis interdum. Ut id semper eros, in lobortis diam. Nam consequat, dolor pharetra imperdiet finibus, lacus turpis tincidunt velit, ut fringilla ligula orci et justo. Praesent sagittis lectus eu metus faucibus aliquam. Donec sollicitudin porttitor massa a mollis. Nulla eleifend orci lacus, et tristique dui viverra eu. Sed nec mollis ligula. Quisque eu tellus libero. Nulla id hendrerit mauris.",0)
+	elif(i == 3):
+		tie.set_color(Color(0.3,1,1))
+		# Schedule an Input in the buffer, after all
+		# the text before it is displayed
+		tie.buff_text("Hey there!! What's your name?\n", 0.01)
+		tie.buff_input()
 	tie.set_state(tie.STATE_OUTPUT)
+
+func _ready():
+	# Add the demos in the list
+	get_node("demos_list").set_focus_mode(0)
+	get_node("demos_list").add_item("No demo")
+	get_node("demos_list").add_item("DEMO_Music")
+	get_node("demos_list").add_item("DEMO_Ipsum")
+	get_node("demos_list").add_item("DEMO_Input")
+	
+	# Connect every signal to check them using the "print()" method
+	tie.connect("input_enter", self, "_on_input_enter")
+	tie.connect("buff_end", self, "_on_buff_end")
+	tie.connect("state_change", self, "_on_state_change")
+	tie.connect("enter_break", self, "_on_enter_break")
+	tie.connect("resume_break", self, "_on_resume_break")
+	tie.connect("tag_buff", self, "_on_tag_buff")
+	pass
+
+func _on_demos_list_item_selected( ID ):
+	select_demo(ID)
+
+func _on_input_enter(s):
+	print("Input Enter ",s)
+	
+	tie.add_newline()
+	tie.buff_text("Oooh, so your name is " + s + "? What a beautiful name!", 0.01)
+	
+	pass
+
+func _on_buff_end():
+	print("Buff End")
+	pass
+
+func _on_state_change(i):
+	print("New state: ", i)
+	pass
+
+func _on_enter_break():
+	print("Enter Break")
+	pass
+
+func _on_resume_break():
+	print("Resume Break")
+	pass
+
+func _on_tag_buff(s):
+	print("Tag Buff ",s)
+	pass
