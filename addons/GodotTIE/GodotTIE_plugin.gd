7c8e7a807f40d370b5c2e9560e985f75e8ea2ca9
dalton5000 <marius.stieme@gmail.com>
2018-03-10 21:02:26 +0100

    Add files via upload
    Port for Godot 3.0.2

--- addons/GodotTIE/GodotTIE_plugin.gd
+++ addons/GodotTIE/GodotTIE_plugin.gd
@@ -4,7 +4,7 @@ extends EditorPlugin
 func _enter_tree():
 	# When this plugin node enters tree, add the custom type
 
-	add_custom_type("TextInterfaceEngine","ReferenceFrame",preload("res://addons/GodotTIE/text_interface_engine.gd"),preload("res://addons/GodotTIE/GodotTIE_icon.png"))
+	add_custom_type("TextInterfaceEngine","ReferenceRect",preload("res://addons/GodotTIE/text_interface_engine.gd"),preload("res://addons/GodotTIE/GodotTIE_icon.png"))
 
 func _exit_tree():
 	# When the plugin node exits the tree, remove the custom type
